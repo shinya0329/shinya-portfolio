e9ac539f6cbb747531228b0a7cdd1adbb4d3f727
Ubuntu <ubuntu@ip-172-31-29-137.ap-northeast-1.compute.internal>
2025-11-28 06:00:06 +0000

    新規投稿のバリデーションの設定

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'kaminari','~> 1.2.1'
+
+gem 'active_storage_validations', '~> 0.8'
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,9 @@ class Post < ApplicationRecord
  
     has_many :favorites, dependent: :destroy
 
+    validates :shop_name, presence: true
+    validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { between: 1.kilobyte..1.megabytes , message: '画像容量が大きすぎます。1MB以下にしてください。' }
+
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
