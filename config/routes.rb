e033c74159d81039dbccf2d06081d662dd49f9e7
Ubuntu <ubuntu@ip-172-31-29-137.ap-northeast-1.compute.internal>
2025-11-28 09:27:52 +0000

    新規登録画面のレイアウト変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  
   devise_for :users
-  root 'homes#top'
+ 
+  root "homes#top"
+  get 'home/about', to: 'homes#about'
+  
+  get '/categorys', to: 'posts#categorys' #カテゴリー一覧ページへのルーティング
+  get 'posts/category/:category', to: 'posts#category' #カテゴリー詳細ページへのルーティング
+  
+  resources :users, only: [:index, :show, :edit, :update]
 
-    resources :posts, only: [:new, :create, :index, :show, :destroy] do
-      resources :post_comments, only: [:create, :destroy]
-      resource :favorites, only: [:create, :destroy]
-    end
+  resources :posts, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 
-    resources :users, only: [:show, :edit, :update]
 end
