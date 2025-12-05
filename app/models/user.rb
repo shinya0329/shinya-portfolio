ff37d135bdf36c8a9dcecb45e9ec70ea64c382dc
Ubuntu <ubuntu@ip-172-31-29-137.ap-northeast-1.compute.internal>
2025-12-05 07:02:25 +0000

    投稿一覧ページ、投稿詳細ページのデザインの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # deviseで使用する機能の記述
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, dependent: :destroy
-  has_many :post_comments, dependent: :destroy 
+         
+  has_many :posts, dependent: :destroy 
+  has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
+  
   has_one_attached :profile_image
-
+  
   validates :name, presence: true
-  validates :profile_image, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { between: 1.kilobyte..1.megabytes , message: '画像容量が大きすぎます。1MB以下にしてください。' }
+  validates :profile_image, content_type: ['image/png', 'image/jpeg'], size: { between: 1.kilobyte..1.megabytes , message: '画像容量が大きすぎます。1MB以下にしてください。' }
 end
